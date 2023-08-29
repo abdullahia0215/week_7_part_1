c2dc752234f44fd808a75c226a985375ef9effb6
Abdi <abdullahia0215@gmail.com>
2023-08-29 07:44:44 -0500

    added a couple of scripts

---
+++ part _2/script110.js
@@ -0,0 +1,11 @@
+function compareNumbers(num1, num2) {
+    if (num1 > num2) {
+      return "The first number was bigger!";
+    } else if (num2 > num1) {
+      return "The second number was bigger!";
+    } else {
+      return "The numbers are the same!";
+    }
+  }
+  
+
